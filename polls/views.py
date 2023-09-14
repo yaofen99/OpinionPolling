10b8ca72dbaadc11e6f267d75d49ab80c14693c7
Qiong Huang <yaofen233@gmail.com>
2023-09-14 13:40:24 -0700

    add test

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import render
 from django.http import Http404
