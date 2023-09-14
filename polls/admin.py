e11adcdd6c81a25264e0d7e932aa7a0d4dc1fb70
Qiong Huang <yaofen233@gmail.com>
2023-09-14 15:17:07 -0700

    adjust templates

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,25 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
 
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ["pub_date", "question_text"]
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+
+
+admin.site.register(Question, QuestionAdmin)
